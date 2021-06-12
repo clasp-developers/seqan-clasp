f8e125da04e3f3b163f59feb3e35037909687eb7
Christian Schafmeister <chris.schaf@verizon.net>
2021-06-12 10:49:47 -0400

    Added seqan-startup.lisp

--- src/seqan-startup.lisp
+++ src/seqan-startup.lisp
@@ -1,2 +1,4 @@
-(load "packages.lisp")
-(load "seqan.lisp")
+(format t "About to load seqan-clasp lisp~%")
+(load "source-dir:extensions;seqan-clasp;src;packages.lisp")
+(load "source-dir:extensions;seqan-clasp;src;seqan.lisp")
+(format t "Done load seqan-clasp lisp~%")
